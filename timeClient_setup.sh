22005ad666175f27737654aebd89291de69f830c
AlexMotz <alexmotz@me.com>
2017-04-24 17:09:03 -0400

    fixing problems

--- timeClient_setup.sh
+++ timeClient_setup.sh
@@ -19,32 +19,32 @@ else
 fi
 # --------------------------------------------------------
 
-# echo "** System - Update and Upgrade **"
-# apt-get update
-# apt-get upgrade -y
 
-# git://anonscm.debian.org/collab-maint/chrony.git
+DEB_REPO_SOURCE_LIST='/etc/apt/sources.list'
+# ---------- Begin Config File ----------
+cat > $DEB_REPO_SOURCE_LIST << EOF
+deb http://archive.raspbian.org/raspbian jessie main contrib non-free rpi
+EOF
+# ----------- End Config File -----------
+
+
+echo "** System - Update and Upgrade **"
+apt-get update
+apt-get upgrade -y
 
 
 echo "** Stopping NTP Daemon**"
 NTP_SYS_PATH='/etc/init.d/ntp'
 $NTP_SYS_PATH stop
-apt-get remove ntp -y
+# apt-get --purge remove ntp -y
 
 
-echo "** Installing Chrony **"
-# apt-get install chrony -y
+echo "** Installing Chrony + Dependencies **"
 
-cd /tmp
-
-wget http://http.us.debian.org/debian/pool/main/libt/libtomcrypt/libtomcrypt0_1.17-6_armhf.deb
-dpkg libtomcrypt0_1.17-6_armhf.deb
-
-wget http://http.us.debian.org/debian/pool/main/t/timelimit/timelimit_1.8-1_armhf.deb
-dpkg -i timelimit_1.8-1_armhf.deb
-
-wget http://http.us.debian.org/debian/pool/main/c/chrony/chrony_1.30-2+deb8u2_armhf.deb
-dpkg -i chrony_1.30-2+deb8u2_armhf.deb
+apt-get install libtomcrypt0 -y
+apt-get install libtommath0 -y
+apt-get install timelimit -y
+apt-get install chrony -y
 
 echo "** Chrony Install Complete **"
 
