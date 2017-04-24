1293a3e05d78512a56a0deab425b330c606844a4
AlexMotz <alexmotz@me.com>
2017-04-24 16:09:06 -0400

    installs dependencies etc

--- timeClient_setup.sh
+++ timeClient_setup.sh
@@ -23,11 +23,13 @@ fi
 # apt-get update
 # apt-get upgrade -y
 
+# git://anonscm.debian.org/collab-maint/chrony.git
+
 
 echo "** Stopping NTP Daemon**"
 NTP_SYS_PATH='/etc/init.d/ntp'
 $NTP_SYS_PATH stop
-apt-get --purge remove ntp -y
+apt-get remove ntp -y
 
 
 echo "** Installing Chrony **"
@@ -35,17 +37,14 @@ echo "** Installing Chrony **"
 
 cd /tmp
 
-wget http://http.debian.net/debian/pool/main/c/chrony/chrony_1.30.orig.tar.gz
-tar -zxvf chrony_1.30.orig.tar.gz
-cd chrony-1.30
+wget http://http.us.debian.org/debian/pool/main/libt/libtomcrypt/libtomcrypt0_1.17-6_armhf.deb
+dpkg libtomcrypt0_1.17-6_armhf.deb
 
-./configure
+wget http://http.us.debian.org/debian/pool/main/t/timelimit/timelimit_1.8-1_armhf.deb
+dpkg -i timelimit_1.8-1_armhf.deb
 
-make
-make install
-
-# wget http://http.us.debian.org/debian/pool/main/c/chrony/chrony_1.30-2+deb8u2_armhf.deb
-# dpkg -i chrony_1.30-2+deb8u2_armhf.deb
+wget http://http.us.debian.org/debian/pool/main/c/chrony/chrony_1.30-2+deb8u2_armhf.deb
+dpkg -i chrony_1.30-2+deb8u2_armhf.deb
 
 echo "** Chrony Install Complete **"
 
